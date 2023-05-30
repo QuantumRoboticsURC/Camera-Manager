bd121c287f3b24e0c15751283944ccac3e5c7f9d
AriadnaHCTec <A01749161@tec.mx>
2023-05-30 16:14:06 -0600

    service

--- camera_manager/scripts/server_camera.py
+++ camera_manager/scripts/server_camera.py
@@ -3,18 +3,23 @@ import rospy
 from camera_manager.srv import camera_photo, camera_photoResponse
 from sensor_msgs.msg import CompressedImage
 
+d=CompressedImage()
 
 def callback(data):
+    print("callback")
     global d 
-    d= data
+    d = data
+   
 
-def camPicture():
+def camPicture(req):
+    global d
+    print("service")
     return camera_photoResponse(d)
 
 def server():
-    print("Entering server")
+    print("Entering server ")
     rospy.init_node("camera_server")
-    rospy.Subscriber("/usb_camera/image_raw/compressed",CompressedImage,callback)
+    rospy.Subscriber("/usb_cam/image_raw/compressed",CompressedImage,callback)
     s = rospy.Service("camera_info",camera_photo, camPicture)
     rospy.spin()
 
